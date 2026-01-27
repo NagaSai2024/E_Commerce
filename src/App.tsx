6fa8659a117d45de207d5b36d583a66e3bb69144
NagaSai <saichinnala9@gmail.com>
2026-01-27 12:16:10 +0530

    Starting the homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,8 @@
-import AccordionDemo from "./examples/AccordionDemo"
-import AlertDialogDemo from "./examples/AlertDialogDemo"
-import CardDemo from "./examples/CardDemo"
-import ChartDemo from "./examples/ChartDemo"
 import Demos from "./examples/Demos"
-import DropDownMenuDemo from "./examples/DropDownMenuDemo"
-import FieldDemo from "./examples/FieldDemo"
-import NagivationDemo from "./examples/NavigationDemo"
-import PaginationDemo from "./examples/PaginationDemo"
-import UserButton from "./examples/UserButton"
-import UserSkeleton from "./examples/UserSkeleton"
 
 function App() {
   return (
     <div className="flex justify-center mt-8">
-      {/* <UserButton /> */}
-      {/* <UserSkeleton/> */}
-      {/* <PaginationDemo /> */}
-      {/* <AccordionDemo /> */}
-      {/* <AlertDialogDemo /> */}
-      {/* <DropDownMenuDemo /> */}
-      {/* <CardDemo /> */}
-      {/* <ChartDemo /> */}
-      {/* <FieldDemo /> */}
-      {/* <NagivationDemo /> */}
       <Demos />
     </div>
   )
